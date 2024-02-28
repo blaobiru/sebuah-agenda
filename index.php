b646a156ca88fbf1e7a6561899c39fc7a4bdc07f
blaobiru <tirlamen@gmail.com>
2024-02-28 12:05:50 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+require 'fungsi.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sebuah agenda</title>
+</head>
+<body>
+    <h1>Selamat datang di sebuah agenda</h1>
+
+    <table border = "1" cellpadding= "50" cellspacing="0">
+
+        <tr>
+            <th>Mod</th>
+            <th>Tanggal</th>
+            <th>Bulan</th>
+            <th>Tahun</th>
+            <th>Ket.</th>
+        </tr>
+
+    </table>
+</body>
+</html>
